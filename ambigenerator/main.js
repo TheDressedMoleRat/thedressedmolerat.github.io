699c88ef59bb859a8f2e4224fcbf15b7515c0c67
Alvar <alvardepaulis@gmail.com>
2024-01-22 19:52:38 +0100

    Fixed scrolling

--- ambigenerator/main.js
+++ ambigenerator/main.js
@@ -15,7 +15,8 @@ function spawnCanvas(width, height) {
 	var ctx = canvas.getContext('2d');
 	canvas.width = width;
 	canvas.height = height;
-	document.body.appendChild(canvas);
+	const canvases = document.getElementById("canvases");
+	canvases.appendChild(canvas);
 	ctx.fillStyle = '#def';
 	ctx.fillRect(0,0,width,height);
 	return canvas;
