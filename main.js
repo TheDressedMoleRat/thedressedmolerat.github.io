e3ced355ce1708239606055b07e68f29775b4788
Alvar <alvardepaulis@gmail.com>
2025-04-17 19:50:34 +0200

    remade the css and added a third theme

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Ok if you want you can read the splash texts here, but where's the fun in that?? Just click on them instead it cycles don't worry.
 const splash_texts = 
-`Don't translate soko ma pipi to english!! wait...
+`Don't say "two eyes wow" in toki pona, worst mistake of my life
 Did you know proportional fonts are literally better for coding?
 Do you ever have several songs at the same time mashupping in your head?
 Oiler 'discovering' a number (literally 3)
 Did you know mole-rats are blind? Ok
-Peanits schmeanits.
+Peanits schmeanits
 So what are yall's genders?
-Oh mama.
+Oh mama
 My friends though I was joking when I said there was a font subreddit :(
 Self hosting? In this economy?!
 Welcome to thoughts for your thoughts
@@ -30,7 +30,6 @@ the pink one so terrify
 Un petit d'un petit. S'étonne aux halles.
 Microsoft is a crazy name if you think about it. ew sorry
 Unattractive people in relationships far away from you!!
-Fargone Faceferve <3 (jKwrYS2a8zA?t=482)
 Orion's 'sword'
 pu-rist jan Usawi be like wan, tu, tu wan, tu tu, luka, luka wan, likujo!
 IP-address? They know where I pee?
@@ -40,8 +39,7 @@ Exactly half of the latin alphabet's minuscules fit in x-height.
 My give a fucks are on vacation. Just kidding I care <3
 A broken record is still right maybe it depends I guess
 If you rearrange the letters in Peru, you could spell Europe.
-T**** N** R****
-Don't say 'Two eyes, wow' in toki pona, worst mistake of my life
+T***s N*w R***n
 Woo gays, gay it up
 Oh ok Nguyen is pronounced ŋwiən apparently??
 Live laugh love? Honey. Die cry hate. (don't)
@@ -51,18 +49,36 @@ Crazy how there's that one donut topology joke and no other ones but what even i
 I am the truest repairman
 Zoo-Wee Mama
 I feel like an umbrella (quoIzf48YeM?t=172)
-It's giving dirt girl from dirt girl (2leQfHW9sX4?t=1075)
 sina toki pona ala la, o moli
 Gazebo? Gazoinksbo
 Bosskeep Gatelight Gasgirl
 Light Keep Boss
-I don't want a history lesson I want a sandwich (dXj97cFU1VM?t=238)
 I could lie, say I likey-like that
 Never heard of the dressed mole-rat? You are one of today's lucky 10,000
+It's giving dirt girl from dirt girl (2leQfHW9sX4?t=1075)
 When I risk it for a biscuit then I'm in a good mood
 använd piltangenterna för att styra din figur <3
 I love drinking soowi tello wawa kəpecken nəmacko en cooleh I.O. kasi
-Ok but what reason is there to pronounce a K in et cetera? Where is it??`.split("\n")
+Ok but what reason is there to pronounce a K in et cetera? Where is it??
+"I" love randomly inserting quotation marks.
+Oh no! Come on, girl. - Terry Mennanecker
+Ugh, food for dinner, again?
+Exercise? I thought you said extra fries!
+my baby loves his apparatus (cAeYMYkew_k?t=2m10s)
+Is it true you went to school with skinny but pretty voice totally tattoo all over da body but pretty drugs taking singing miss wine in the house
+That's numberwang!
+Hi mom, I wish I could come back! (Wm6BuznFKHY?t=980)
+τ > π
+Shortening <i>there are</i> to <i>there's</i> unnerves me slightly.
+sadly I think these splash texts are where most of this websites effort lies
+Oh yeah that classic turn that CLEARLY says "Hurry, boy, it's waiting there for you"
+But acerooolaaaa
+Boyfriend skinner <3
+Lelda, Danongorf, and Zink
+Americans be like yeah I love studying mathematic
+Apparently Denmark grades 12, 10, 7, 4, 02, 00, -3 instead of A-F
+I'm trying to be linguistically descriptivistic but oh brother
+In Swedish "fishes" and "fishers" are the same word for some reason`.split("\n")
 
 let splash_index = Math.floor(Math.random() * splash_texts.length);
 const splash_element = document.getElementById("splash");
@@ -74,7 +90,7 @@ const themes = {
 			main_bg: '#282a36',
 			secondary_bg: '#44475A',
 			text: '#F8F8F2',
-			links: '#8BE9FD',
+			links: '#ffaad5',
 			splash: '#6272A4'
 		},
 		"bg-image":"/media/images/backgrounds/tokipona.png"
@@ -89,6 +105,16 @@ const themes = {
 		},
 		"bg-image":"/media/images/backgrounds/white.png"
 	},
+	space: {
+		"vars":{
+			main_bg: 'black',
+			secondary_bg: '#333',
+			text: 'white',
+			links: '#c7f',
+			splash: '#555'
+		},
+		"bg-image":"/media/images/backgrounds/spacebg.gif"
+	}
 };
 
 function splash() {
@@ -102,7 +128,14 @@ function set_theme(theme_dictionary) {
 		root.style.setProperty("--"+var_name, color);
 	}
 
-	document.getElementById("bg").style.backgroundImage = "url("+theme_dictionary["bg-image"]+")";
+	let bg_element = document.getElementById("bg")
+	bg_element.style.backgroundImage = "url("+theme_dictionary["bg-image"]+")";
+
+	if (theme_dictionary["bg-image"].endsWith(".gif")) {
+		bg_element.style.animation = "none";
+	} else {
+		bg_element.style.animation = "6s linear infinite bg";
+	}
 }
 
 function cycle_theme() {
