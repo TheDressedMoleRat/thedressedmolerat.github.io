6316ebdf33c0050e2ffc13cdc2b206529e3619b6
Alvar <alvardepaulis@gmail.com>
2026-02-06 20:17:43 +0100

    removed christmas theme and added answer boxes for the cryptics

--- main.js
+++ main.js
@@ -133,20 +133,6 @@ const theme_dicts = [
 		bg_image: "url(/media/images/backgrounds/tokipona.webp)",
 		bg_animation: "6s linear infinite bg",
 	},
-	{ // christmas
-		bg: '#ff4141ff',
-		box_bg: '#f02f2fff',
-		secondary_bg: '#f5706e',
-		text: '#F8F8F2',
-		links: '#5cd64cff',
-		button_text: 'white',
-		splash: '#ffb0b0ff',
-		body_font: "Atkinson Hyperlegible Next, sans-serif",
-		heading_font: "Kalnia, serif",
-		box_rounding: "35px",
-		bg_image: "url(/media/images/backgrounds/christmas.png)", //"repeating-linear-gradient(45deg, #f5706e, #f5706e 30px, #ec2b2b 30px, #f33 60px)",
-		bg_animation: "6s linear infinite bg"
-	},
 	{ // light
 		bg: 'white',
 		box_bg: 'white',
